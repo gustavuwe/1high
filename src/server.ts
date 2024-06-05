029f8b6da1ae2f9bc9f3d71799d19dfff202b7a1
Gustav Uwe <gustavuwe.123@gmail.com>
2024-06-05 17:07:54 -0300

    test husky again

--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,37 @@
-import 'dotenv/config';
-import Fastify from 'fastify';
-import { usersRepositories } from './repositories/users.repositories';
+import 'dotenv/config'
+import Fastify from 'fastify'
+import { usersRepositories } from './repositories/users.repositories'
 
 interface RequestUser {
-  email: string;
-  username: string;
-  password: string;
+  email: string
+  username: string
+  password: string
 }
 
-console.log(process.env.DATABASE_URL);
+console.log(process.env.DATABASE_URL)
 
-const app = Fastify({});
+const app = Fastify({})
 
 app.get('/', (request, reply) => {
-  reply.send({ hello: 'world' });
-});
+  reply.send({ hello: 'world' })
+})
 
 app.post('/register', async (request, reply) => {
-  const { email, username, password } = request.body;
+  const { email, username, password } = request.body
 
   try {
-    usersRepositories.createUser(email, username, password);
+    usersRepositories.createUser(email, username, password)
   } catch (err) {
-    return reply.status(500).send(err);
+    return reply.status(500).send(err)
   }
 
-  return reply.status(201).send();
-});
+  return reply.status(201).send()
+})
 
 app.listen({ port: 3000 }, (err, adress) => {
   if (err) {
-    app.log.error(err);
-    process.exit(1);
+    app.log.error(err)
+    process.exit(1)
   }
-  console.log(`Server is Running on ${adress}`);
-});
+  console.log(`Server is Running on ${adress}`)
+})
